f0dd35d3a73b983732ccce915671d58766ee1571
zenenperaza <peraza@outlook.com>
2023-07-13 12:28:33 -0400

    testing e2e cn cypress - coumter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <h1>Counter App</h1>
+<h3 id="counter-info">{{ counter }}</h3>
+<button v-on:click="counter++">Incrementar</button>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import { ref  } from "vue";
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+const counter = ref(0)
+
 </script>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,19 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Counter Feature', () => {
+  it('User can Acces to page', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.contains('h1', 'Counter App')
   })
+  it('User can increment counter', () => {
+    cy.visit('/')
+    cy.get('#counter-info').contains('0')
+    cy.contains('button', 'Incrementar').click()
+    cy.get('#counter-info').contains('1')
+  })
+
+
+
+
 })
+
